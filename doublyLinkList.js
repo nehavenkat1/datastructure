7243de6049afff79ad480d10cbee5cd6083d6929
nehavenkat1 <nehavenky@yahoo.com>
2021-08-15 08:14:35 +0530

    new mothods added

--- doublyLinkList.js
+++ doublyLinkList.js
@@ -25,6 +25,94 @@ function DoublyLinkList() {
         }
         this.size++
     }
+
+    this.addAtTail = function(value) {
+        if(this.tail === null) {
+            this.tail = new DoublyLinkListNode(value)
+            this.head = this.tail
+        } else {
+            let temp = new DoublyLinkListNode(value)
+            temp.prev = this.tail
+            this.tail.next = temp
+            this.tail = temp
+        }
+        this.size++
+    }
+
+    this.addAtPosition = function(value, position) {
+        let curPos = 0
+        if(position <= 0) return this.addAtFront(value)
+        if(position === this.size || position > this.size) return this.addAtTail(value)
+        else {
+            let temp = this.head
+            let previous
+            while(curPos !== position) {
+                previous = temp
+                temp = temp.next
+                curPos++
+            }
+            let newNode = new DoublyLinkListNode(value)
+            newNode.prev = previous
+            newNode.next = temp
+            previous.next = newNode
+            temp.prev = newNode
+            this.size++ 
+        }
+    }
+
+    this.deleteAtHead = function() {
+        let toReturn = null
+        if(this.head !== null) {
+            toReturn = this.head.data
+            if(this.head === this.tail) {
+                this.head = null
+                this.tail = null
+            } else {
+                this.head = this.head.next
+                this.head.prev = null
+            }
+            this.size--
+        }
+        return toReturn
+    }
+
+    this.deleteAtTail = function() {
+        let toReturn = null
+        if(this.tail !== null) {
+            if(this.head === this.tail) {
+                this.head = null
+                this.tail = null
+            } else {
+                this.tail = this.tail.prev
+                this.tail.next = null
+            }
+            this.size--
+        }
+        return toReturn
+    }
+
+    this.deleteAtPosition = function(pos) {
+        console.log(this.size, pos)
+        if(pos >= 0 && pos <= this.size) {
+            if(pos === 0) return this.deleteAtHead()
+            if(pos === this.size) return this.deleteAtTail()
+            else {
+                let index = 0
+                let current = this.head
+                let previous
+                while(index !== pos) {
+                    previous = current
+                    current = current.next
+                }
+                previous.next = current.next
+                this.size--
+            }
+        } else return null
+    }
+
+    this.length = function() {
+        return this.size
+    }
 }
 
 let d1 = new DoublyLinkList()
@@ -32,4 +120,10 @@ d1.addAtFront(11)
 d1.addAtFront(22)
 d1.addAtFront(33)
 d1.addAtFront(44)
+d1.addAtTail(111)
+d1.addAtTail(222)
+d1.addAtPosition(101, 1)
+//console.log(d1)*/
+//d1.deleteAtTail()
+d1.deleteAtPosition(6)
 console.log(d1)
